3c66dfc761ae8d23a9e5dfaede6b54d80fea32b6
Leo Kim <me@leok.kr>
2021-11-13 21:28:27 +0900

    feat: get redirect_uri from cookie

--- routes/v1/callback.py
+++ routes/v1/callback.py
@@ -1,9 +1,18 @@
+from typing import Optional
 from fastapi import APIRouter
+from fastapi.params import Cookie
+from models.user import User, parse_google_response
 from utils.google import get_token, get_user_info
 
 callback_router = APIRouter()
 
 
-@callback_router.get("/google")
-async def callback_google(code: str):
-    return await get_user_info((await get_token(code)).get("access_token"))
+@callback_router.get("/google", response_model=User)
+async def callback_google(code: str, hyundai_id_callback: Optional[str] = Cookie(None)):
+    # print(hyundai_id_callback)
+    user = User.parse_obj(
+        parse_google_response(
+            await get_user_info((await get_token(code)).get("access_token"))
+        )
+    )
+    return user
