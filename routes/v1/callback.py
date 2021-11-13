042acabcd69cc8f9ccc62a10d34d7667dfc15f8e
Leo Kim <me@leok.kr>
2021-11-14 03:15:45 +0900

    feat: add md5hash

--- routes/v1/callback.py
+++ routes/v1/callback.py
@@ -1,18 +1,32 @@
 from typing import Optional
 from fastapi import APIRouter
 from fastapi.params import Cookie
+from starlette.responses import JSONResponse, RedirectResponse
 from models.user import User, parse_google_response
 from utils.google import get_token, get_user_info
+from utils.auth import gen_oauth_code
+from os import getenv
 
 callback_router = APIRouter()
 
 
-@callback_router.get("/google", response_model=User)
+@callback_router.get("/google")
 async def callback_google(code: str, hyundai_id_callback: Optional[str] = Cookie(None)):
     # print(hyundai_id_callback)
-    user = User.parse_obj(
-        parse_google_response(
-            await get_user_info((await get_token(code)).get("access_token"))
+    try:
+        user = User.parse_obj(
+            parse_google_response(
+                await get_user_info((await get_token(code)).get("access_token"))
+            )
         )
-    )
-    return user
+    except Exception as e:
+        print(e)
+        return JSONResponse(
+            {
+                "code": "PARSE_ERROR",
+                "message": f"Failed Parsing user informaing from Google. Please retry and contact to: {getenv('CONTACT')}",
+            },
+            status_code=500,
+        )
+        
+    return RedirectResponse(hyundai_id_callback + f"?code={gen_oauth_code(user.id).get('code')}")
---
+++ utils/etc.py
@@ -0,0 +1,4 @@
+import hashlib
+
+def md5hash(string: str):
+    return hashlib.md5(string.encode('utf-8')).hexdigest()
