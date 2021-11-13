5731c222ea3d3b49d6988fdb761a16cc109d6f32
Leo Kim <me@leok.kr>
2021-11-14 03:15:03 +0900

    chore: rename login router to auth router

--- routes/v1/__init__.py
+++ routes/v1/__init__.py
@@ -2,15 +2,15 @@ from fastapi import APIRouter
 
 # from .users import users_router
 from .callback import callback_router
-from .login import login_router
+from .auth import auth_router
 
-#from .test import test_router
+# from .test import test_router
 
 v1_router = APIRouter()
 
 # v1_router.include_router(users_router, prefix="/users", tags=["users"])
-#v1_router.include_router(test_router, prefix="/test", tags=["test"])
+# v1_router.include_router(test_router, prefix="/test", tags=["test"])
 
 
 v1_router.include_router(callback_router, prefix="/callback", tags=["callback"])
-v1_router.include_router(login_router, tags=["login"])
+v1_router.include_router(auth_router, tags=["auth"])
