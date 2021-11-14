8baa472c3d92abe39b9aa717cf3223a692e55547
Leo Kim <me@leok.kr>
2021-11-14 16:14:08 +0900

    feat: client id/secret tool

---
+++ utils/register_client_secret.py
@@ -0,0 +1,23 @@
+from os import getenv
+from datetime import datetime
+from etc import md5hash
+from redis import Redis
+from dotenv import load_dotenv
+import hashlib
+load_dotenv()
+
+client_credentials_db = Redis(host=getenv("REDIS_HOST"), port=getenv("REDIS_PORT"), db=2)
+
+def md5hash(string: str):
+    string = str(string)
+    return hashlib.md5(string.encode('utf-8')).hexdigest()
+
+def register():
+    print("--HyundaiID oAuth Client Secret Register Tool--")
+    id = input("Please enter the Client ID: ")
+    secret = md5hash(int(md5hash(id),16)+int(datetime.now().timestamp()))
+    client_credentials_db.set(id, secret)
+    print("User registered with Client Secret: ", secret)
+
+if __name__ == '__main__':
+    register()
