dff1f4d1f3b7a600651b68d508539af8e3da4e4f
Leo Kim <me@leok.kr>
2021-11-14 16:13:26 +0900

    feat: add test callback route

--- routes/v1/__init__.py
+++ routes/v1/__init__.py
@@ -1,16 +1,18 @@
 from fastapi import APIRouter
 
-# from .users import users_router
-from .callback import callback_router
 from .auth import auth_router
 
-# from .test import test_router
+# from .users import users_router
+from .callback import callback_router
+from .test import test_router
+from .users import user_router
 
 v1_router = APIRouter()
 
 # v1_router.include_router(users_router, prefix="/users", tags=["users"])
-# v1_router.include_router(test_router, prefix="/test", tags=["test"])
+v1_router.include_router(test_router, prefix="/test", tags=["test"])
 
 
 v1_router.include_router(callback_router, prefix="/callback", tags=["callback"])
 v1_router.include_router(auth_router, tags=["auth"])
+v1_router.include_router(user_router, prefix="/users", tags=["users"])
